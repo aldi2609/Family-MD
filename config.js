b97a097fdf3c7d1e4b2522ff5a485def6d6252a7
aldi2609 <109309282+aldi2609@users.noreply.github.com>
2022-07-16 15:20:37 +0700

    Update config.js

--- config.js
+++ config.js
@@ -43,11 +43,11 @@ global.eror = '_*мααƒ ѕєяνєя тι∂αк ѕтαвιℓ..*_'
 
 //========Url Template Buttons=========//
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/aguzfamilia"
+global.urlnya = "https://www.instagram.com/aldii_260"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 899-9625-005'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
@@ -57,8 +57,8 @@ global.dikit = "dikit lagi, semangat ya :')"
 global.multiplier = 100 // The higher, The harder levelup
 
 // Sticker WM
-global.packname = data.namabot;
-global.author = data.owner;
+global.packname = Aldi
+global.author = Aldi Bot
 
 //=========== Requirements ==========//  
         global.colong1 = 'Ciss 📸'
